542708a94690f0e73e3c8b059664d68cb9419dee
Marlen-MC <69117034+Marlen-MC@users.noreply.github.com>
2022-01-17 21:24:31 -0600

    Add files via upload

---
+++ MJC-main/index.js
@@ -0,0 +1,40 @@
+var students = [
+{
+    lname: "Reyes",
+    name: "Celso",
+    age: 21,
+    birthday: {
+      year: 2000,
+      month: "Aprill",
+      day: 26
+    },
+    Career: "IDYGS"
+  }, 
+  {
+      lname: "Cruz",
+      name: "Marlen",
+      age: 22,
+      birthday: {
+        year: 1999,
+        month: "August",
+        day: 14
+      },
+      Career: "IDYGS"
+    }
+  ]
+
+var table = document.getElementsByTagName("table")[0];
+for (var i = 0; i < students.length; i++) {
+  var templ = document.getElementById("tbodytempl").content.cloneNode(true);
+  var tdnumber = templ.querySelector("span[name='tbodynumber']");
+  var tdname = templ.querySelector("span[name='tbodyname']");
+  var tdage = templ.querySelector("span[name='tbodyage']");
+  var tdcareer = templ.querySelector("span[name='tbodycareer']");
+
+  tdnumber.innerText = i + 1;
+  tdname.innerText = students[i].lname + " " + students[i].name;
+  tdage.innerText = students[i].age;
+  tdcareer.innerText = students[i].Career;
+
+  table.append(templ);
+}
